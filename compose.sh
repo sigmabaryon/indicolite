d25ba15428f26f75baccec49b81f49e1f8ccc51a
sigmabaryon <shreysonar13@gmail.com>
2022-08-16 01:35:52 +0530

    Add glu

--- compose.sh
+++ compose.sh
@@ -9,6 +9,7 @@ if [ ! -d $REPO/objects ]; then
     ostree --repo=$REPO init --mode=archive-z2
 fi
 
-rpm-ostree compose tree --unified-core --cachedir=$CACHE --repo=$REPO --ex-lockfile=fedora-indicolite-lock.overrides.yaml  fedora-indicolite.yaml
+#rpm-ostree compose tree --unified-core --cachedir=$CACHE --repo=$REPO --ex-lockfile=fedora-indicolite-lock.overrides.yaml  fedora-indicolite.yaml
+rpm-ostree compose tree --unified-core --cachedir=$CACHE --repo=$REPO fedora-indicolite.yaml
 
 ostree --repo=$REPO summary -u
